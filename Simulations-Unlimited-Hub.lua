42c4a763406046b2e3143d953bcd4fa9f09dbc82
simulatorwinner <haxorus.bot@gmail.com>
2022-02-22 09:23:41 +0530

    Re-uploaded All Scripts
    As SU Hub has a new owner and the github has changed due to this

---
+++ Simulations-Unlimited-Hub.lua
@@ -0,0 +1,24 @@
+gameId = game.PlaceId
+
+if gameId == 1499872953 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/simulatorwinner/scripts/main/Boku-no-roblox-GUI.lua"))()
+elseif gameId == 3956818381 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/simulatorwinner/scripts/main/Ninja-Legends-GUI.lua"))()
+elseif gameId == 2636441885 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/simulatorwinner/scripts/main/Speed-City-GUI-V2.lua"))()
+elseif gameId == 2619187362 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/simulatorwinner/scripts/main/Super-Power-Fighting-Sim-GUI.lua"))()
+elseif gameId == 7485253727 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/simulatorwinner/scripts/main/True-Piece-GUI.lua"))()
+elseif gameId == 6085586197 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/simulatorwinner/scripts/main/Project-Hero-GUI.lua"))()
+else
+	local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+	local Notify = AkaliNotif.Notify;
+	Notify({
+		Title = "Game Not Supported";
+		Description = "go to discord.gg/fRpJJCVn8R to check the scripts available";
+		Duration = 3;
+	});
+	setclipboard("discord.gg/fRpJJCVn8R")
+end
